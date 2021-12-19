d2351a4089fe3cbaabc0e320f91b7f697890e086
kalenarndt <kalenarndt@gmail.com>
2021-12-19 22:13:00 +0000

    fix provider issue

--- examples/basic-l3-network/providers.tf
+++ examples/basic-l3-network/providers.tf
@@ -1,7 +1,15 @@
 provider "unifi" {
-
   username       = "terraform"
   password       = "terraform_pass"
   allow_insecure = true
   api_url        = "https://172.16.200.1"
+}
+
+terraform {
+  required_providers {
+    unifi = {
+      source = "paultyng/unifi"
+      version = "0.34.0"
+    }
+  }
 }
